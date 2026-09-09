74acb22f531fb6261624665a1a6a46099549b17d
BINGO <97218846+stardime-bingo@users.noreply.github.com>
2026-09-09 15:36:03 +0800

    Fine-tune menu bar spacing in Polaris 1.4.2

--- build.sh
+++ build.sh
@@ -134,9 +134,9 @@ cat > "$APP_BUNDLE/Contents/Info.plist" << 'PLIST'
     <key>CFBundleIdentifier</key>
     <string>com.bingowu.polaris</string>
     <key>CFBundleVersion</key>
-    <string>23</string>
+    <string>24</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.4.1</string>
+    <string>1.4.2</string>
     <key>CFBundleExecutable</key>
     <string>Polaris</string>
     <key>CFBundleIconFile</key>
